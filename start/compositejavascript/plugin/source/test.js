d48145693d4f4299dec97fbf30ac0c717af76d0d
Ruben Westerberg <rwesterberg@dgrt.com.au>
2018-02-02 11:49:31 +0000

    Added test file

---
+++ start/compositejavascript/plugin/source/test.js
@@ -0,0 +1,15 @@
+`
+	.test{
+		background-size:1px;
+		background-color:#000000;
+		background-repeat:repeat;
+		width:100px;
+		margin-top:10cm;
+		left:1px;
+		top:2cm;
+		padding-bottom:2px;
+		bottom:2px;
+		flex-direction: column;
+
+	}	`
+`
